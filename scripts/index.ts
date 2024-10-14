bce5237c8558a2fb2c5423532601ffde75823f7d
zeriix <wflorentin@myges.fr>
2024-10-14 12:24:08 +0200

    feat(1): adding type unknown

--- scripts/index.ts
+++ scripts/index.ts
@@ -2,6 +2,7 @@ import "./typescriptTransformators/any";
 import "./typescriptTransformators/undefined";
 import "./typescriptTransformators/null";
 import "./typescriptTransformators/void";
+import "./typescriptTransformators/unknown";
 
 export * from "./zodTypescriptTransformator";
 
---
+++ scripts/typescriptTransformators/unknown.test.ts
@@ -0,0 +1,8 @@
+import { ZodTypescriptTransformator } from "@scripts/index";
+import { z as zod } from "zod";
+
+it("unknown", () => {
+	const result = ZodTypescriptTransformator.convert(zod.unknown());
+
+	expect(result).toBe("type Zod2ts_1_duplojs = unknown;");
+});
